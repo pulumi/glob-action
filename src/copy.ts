bbaf76c5a3a4a5005734b5eadd124f80ac09458e
Daniel Bradley <daniel@pulumi.com>
2024-05-14 11:21:39 +0100

    Add debug logs & outputs
    - Add debug logs for diagnosing issues.
    - Add output of number of files matched by the glob patterns.

--- src/copy.ts
+++ src/copy.ts
@@ -1,3 +1,4 @@
+import * as action from "@actions/core";
 import * as glob from "fast-glob";
 import { copyFileSync, mkdirSync, constants } from "fs";
 import { dirname, join } from "path";
@@ -6,15 +7,21 @@ export async function copy(src: string, paths: string[], dest: string) {
   if (paths.length === 0) {
     paths = ["**"];
   }
+  action.debug(`Expanding paths:\n${paths.join("\n")}`);
   const matches = glob.globStream(paths, { cwd: src, onlyFiles: true });
+  let count = 0;
   for await (const file of matches) {
     const fileStr = Buffer.isBuffer(file) ? file.toString("utf-8") : file;
     const srcPath = join(src, fileStr);
     const destPath = join(dest, fileStr);
     const destDir = dirname(destPath);
+    action.debug(`Copying ${srcPath} to ${destPath}`);
     // Ensure dir exists
     mkdirSync(destDir, { recursive: true });
     // Copy file
     copyFileSync(srcPath, destPath, constants.COPYFILE_EXCL);
+    count++;
   }
+  action.debug(`Copied ${count} files`);
+  return count;
 }
